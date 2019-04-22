b19338782c49dcf5f111462e2649a0ed3bc5be89
ferrisboran <ferris@ferryrules.com>
2019-04-22 13:20:04 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -5,20 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
-    #   @@items.each do |item|
-    #     resp.write "#{item}\n"
-    #   end
-    # elsif req.path.match(/search/)
-    #
-    #   search_term = req.params["q"]
-    #
-    #   if @@items.include?(search_term)
-    #     resp.write "#{search_term} is one of our items"
-    #   else
-    #     resp.write "Couldn't find #{search_term}"
-    #   end
-    # if req.path.match(/items/)
+    
     if req.path.match(/items/)
       item_search = req.path.split("/items/").last
       item = @@items.find { |i| i.name == item_search }
@@ -36,34 +23,3 @@ class Application
   end
 
 end
-
-
-# class Application
-#
-#   @@items = ["Apples","Carrots","Pears"]
-#
-#   def call(env)
-#     resp = Rack::Response.new
-#     req = Rack::Request.new(env)
-#
-#     if req.path.match(/items/)
-#       @@items.each do |item|
-#         resp.write "#{item}\n"
-#       end
-#     elsif req.path.match(/search/)
-#
-#       search_term = req.params["q"]
-#
-#       if @@items.include?(search_term)
-#         resp.write "#{search_term} is one of our items"
-#       else
-#         resp.write "Couldn't find #{search_term}"
-#       end
-#
-#     else
-#       resp.write "Path Not Found"
-#     end
-#
-#     resp.finish
-#   end
-# end
