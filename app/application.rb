3e486a06b4858306bd394325cd5614274ac8131b
Edgar Ong <emailedgar@gmail.com>
2019-04-21 21:04:52 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,35 @@
+require 'pry'
+
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    # Your application should only accept the /items/<ITEM NAME> route. Everything else should 404
+    if req.path.match(/items/)
+
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
+
+      # IF a user requests an item that you don't have, then return a 400 and an error message
+      if item.nil?
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        # If a user requests /items/<Item Name> it should return the price of that item
+        resp.write item.price
+      end
+
+    else
+
+      resp.write "Route not found"
+      resp.status = 404
+
+    end
+
+    resp.finish
+  end
+end
